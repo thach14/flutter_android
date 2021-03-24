fe0d287b9bdcdfa909a080895c51431721150579
NgocThach <62742480+onelove1816@users.noreply.github.com>
2021-03-24 18:49:22 +0700

    update cac chuc nang them xoa sua task

--- do_an_co_so/flutter_app/lib/Screen/todo_list_screen.dart
+++ do_an_co_so/flutter_app/lib/Screen/todo_list_screen.dart
@@ -1,30 +1,70 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Helper/database_helper.dart';
+import 'package:flutter_app/Model/task_model.dart';
 import 'package:flutter_app/Screen/add_task_screen.dart';
+import 'package:intl/intl.dart';
 
 
-class TodoListScreen extends StatefulWidget {
+class TodoListScreeen extends StatefulWidget {
   @override
-  _TodoListScreenState createState() => _TodoListScreenState();
+  _TodoListScreeenState createState() => _TodoListScreeenState();
 }
 
-class _TodoListScreenState extends State<TodoListScreen> {
-  Widget _buildTask(int index) {
+class _TodoListScreeenState extends State<TodoListScreeen> {
+  Future<List<Task>> _taskList;
+  final DateFormat _dateFormat = DateFormat('MMM dd,yyyy');
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTaskList();
+  }
+
+  _updateTaskList() {
+    setState(() {
+      _taskList = DatabaseHelper.instance.getTaskList();
+    });
+  }
+
+  Widget _buildTask(Task task) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      padding: const EdgeInsets.symmetric(horizontal: 25),
       child: Column(
-        children: <Widget>[
+        children: [
           ListTile(
-            title: Text('Task title'),
-            subtitle: Text('03/11/2021 - High'),
-            trailing: Checkbox(
-              onChanged: (value) {
-                print(value);
-              },
-              activeColor: Theme.of(context).primaryColor,
-              value: true,
+            title: Text(
+              task.title,
+              style: TextStyle(
+                  fontSize: 18,
+                  decoration: task.status == 0
+                      ? TextDecoration.none
+                      : TextDecoration.lineThrough),
             ),
+            subtitle: Text('${_dateFormat.format(task.date)} ${task.priority}',
+                style: TextStyle(
+                    fontSize: 15,
+                    decoration: task.status == 0
+                        ? TextDecoration.none
+                        : TextDecoration.lineThrough)),
+            trailing: Checkbox(
+                onChanged: (value) {
+                  task.status = value ? 1 : 0;
+                  DatabaseHelper.instance.updateTask(task);
+                  _updateTaskList();
+                  print(value);
+                },
+                activeColor: Theme.of(context).primaryColor,
+                value: task.status == 1 ? true : false),
+            onTap: () => Navigator.push(
+                context,
+                CupertinoPageRoute(
+                    builder: (_) => AddTaskSreen(
+                      task: task,
+                      updateTaskList: _updateTaskList,
+                    ))),
           ),
-          Divider(),
+          Divider()
         ],
       ),
     );
@@ -35,45 +75,65 @@ class _TodoListScreenState extends State<TodoListScreen> {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
-        child: Icon(Icons.add),
-        onPressed: () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (_) => AddTaskScreen()),
-        ),
-      ),
-      body: ListView.builder(
-        padding: EdgeInsets.symmetric(vertical: 80.0),
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          if (index == 0)
-            return Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'My Tasks',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      '1 of 10',
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w400),
-                    )
-                  ]),
-            );
-
-          return _buildTask(index);
+        onPressed: () {
+          Navigator.push(
+              context,
+              CupertinoPageRoute(
+                  builder: (_) => AddTaskSreen(
+                    updateTaskList: _updateTaskList,
+                  )));
         },
+        child: Icon(Icons.add),
       ),
+      body: FutureBuilder(
+          future: _taskList,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            final int completedTaskCount = snapshot.data
+                .where((Task task) => task.status == 1)
+                .toList()
+                .length;
+
+            return ListView.builder(
+              padding: EdgeInsets.symmetric(vertical: 80),
+              itemCount: 1 + snapshot.data.length,
+              itemBuilder: (BuildContext context, int i) {
+                if (i == 0) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'My Task',
+                          style: TextStyle(
+                              fontSize: 40,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          '$completedTaskCount of ${snapshot.data.length}',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                  );
+                }
+                return _buildTask(snapshot.data[i - 1]);
+              },
+            );
+          }),
     );
   }
 }
